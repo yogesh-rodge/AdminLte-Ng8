39b264ffa9d5cffd7bc9d98e85e73317d2638fd2
yogeshrodge <yogeshrodge143@gmail.com>
2020-01-11 07:37:21 +0530

    make it better

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppheaderComponent } from './component/appheader/appheader.component';
+import { AppmenuComponent } from './component/appmenu/appmenu.component';
+import { AppfooterComponent } from './component/appfooter/appfooter.component';
+import { AppsettingsComponent } from './component/appsettings/appsettings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppheaderComponent,
+    AppmenuComponent,
+    AppfooterComponent,
+    AppsettingsComponent
   ],
   imports: [
     BrowserModule,
